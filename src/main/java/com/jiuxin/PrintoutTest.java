dc22196f4d3cb13e93c76c74a7f9a20f8d5bd94d
nieqing <nieqing@jiuxiniot.com>
2021-02-24 19:34:57 +0800

    test

--- src/main/java/com/jiuxin/PrintoutTest.java
+++ src/main/java/com/jiuxin/PrintoutTest.java
@@ -5,5 +5,6 @@ import java.util.Arrays;
 public class PrintoutTest {
     public static void main(String[] args) {
         System.out.println("Hello World ! test:" + Arrays.asList(args));
+        System.out.println("Hello Cannal !");
     }
 }
