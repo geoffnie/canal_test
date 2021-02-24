72b848f19e19aeaa80acc6e601b16ee6306cd000
nieqing <nieqing@jiuxiniot.com>
2021-02-24 18:08:35 +0800

    test

---
+++ src/main/java/com/jiuxin/PrintoutTest.java
@@ -0,0 +1,9 @@
+package com.jiuxin;
+
+import java.util.Arrays;
+
+public class PrintoutTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World ! test:" + Arrays.asList(args));
+    }
+}
